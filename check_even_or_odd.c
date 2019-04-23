7e60d7b943aa1c1c2c81919fdc415f720a9092e5
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2019-04-23 16:34:49 +0200

    Create  check_even_or_odd.c

---
+++ check_even_or_odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+ int number;
+ int min,max;
+
+ printf("Enter the minimum range: ");
+ scanf("%d",&min);
+ printf("Enter the maximum range: ");
+ scanf("%d",&max);
+ printf("Odd numbers in given range are: ");
+ for(number = min;number <= max; number++)
+ if(number % 2 !=0)
+ printf("%d ",number);
+
+ return 0;
+}
