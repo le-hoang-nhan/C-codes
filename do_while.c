5eecb8fc1ca8c674a641f98a56d71de6773310f4
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2019-04-23 16:27:49 +0200

    Create do_while.c

---
+++ do_while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int i=0;
+ do
+ {
+ printf("Value of i= %d\n",i);
+ //i=i+1;
+ i++;
+ } while (i > 10);
+ printf("********************\n");
+ int j=0;
+ while (j > 10)
+ {
+ printf("Value of j= %d\n",j);
+ //j=i+1;
+ j++;
+ }
+}
